d342bf303cc8d23b79cbeb65bc9cf4734cc64283
Arnab Dey <32412133+arnabdeypolimi@users.noreply.github.com>
2021-05-26 13:58:35 +0200

    added k

--- test_pose.py
+++ test_pose.py
@@ -13,6 +13,8 @@ dataset_handler = DatasetHandler(2)
 image1 = dataset_handler.images_rgb[0]
 depth1 = dataset_handler.depth_maps[0]
 image2 = dataset_handler.images_rgb[1]
+
+k = dataset_handler.k
 #TODO get the pose matrices using two RGBD images
 
 #TODO print homogenous pose matrix (4x4)
